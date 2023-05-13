49b88463fd5e487d8367435e64efce44347cd5a9
Serhii Halaka <serg.galaka@gmail.com>
2023-05-14 01:51:59 +0300

    movie page with search by parameter

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,31 +1,44 @@
 import { useState, useEffect } from 'react';
 import { apiQuerySearch } from 'services/api';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 import SearchBar from 'components/Searchbar/Searchbar';
 import { ProgressBar } from 'react-loader-spinner';
 import ListItem from 'components/ListItem/ListItem';
 
 export default function Movies() {
-  const [query, setQuery] = useState('');
   const [searchResult, setSearchResult] = useState(``);
   const [isLoading, setIsLoading] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const location = useLocation();
+  console.log(location);
 
   function handleSubmit(value) {
+    console.log(value);
     if (value === ``) {
       alert(`Please enter search query`);
       return;
     }
-    if (value === query) {
-      alert(`You've just searched for the same thing!!!`);
+    if (value === searchParams.get(`title`)) {
+      alert(`Your are looking at the result!`);
       return;
     }
-    setQuery(value);
+    setSearchParams({ title: value });
   }
+
+  // search on param title change
   useEffect(() => {
     const findQuerySearch = async () => {
+      const title = searchParams.get(`title`);
       try {
+        if (!title) {
+          setSearchResult([]);
+          return;
+        }
+
         setIsLoading(true);
-        const { results } = await apiQuerySearch(query);
+        const { results } = await apiQuerySearch(title);
         setSearchResult(results);
       } catch (error) {
         console.log(error);
@@ -34,7 +47,7 @@ export default function Movies() {
       }
     };
     findQuerySearch();
-  }, [query]);
+  }, [searchParams]);
 
   return (
     <div>
@@ -49,5 +62,3 @@ export default function Movies() {
     </div>
   );
 }
-
-// apiQuerySearch;
